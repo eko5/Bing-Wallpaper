cd41d527ac716236528c4c610a989008be32cf43
Piotr Filipek <Piotrek290@gmail.com>
2015-09-17 13:34:29 +0200

    PEP8, removed camelCase, moved __init__ body to other method

--- bingbackground.py
+++ bingbackground.py
@@ -13,28 +13,23 @@ class BingBackground:
 	directory = 'wallpapers'
 
 	def __init__(self):
-		url = self.domain % self.getJSON()['images'][0]['url']
-		name = url.split('/')[-1]
-		self.target = '%s/%s/%s' % (os.path.dirname(os.path.abspath(__file__)), self.directory, name)
-		
-		# check if file exists
-		if not os.path.isfile(self.target):
-			#print "Saving file..."
-			self.save(url)
-		else:
-			#print "File exists..."
-
-		# Set wallpaper
-		#print "Setting wallpaper..."
-		self.setAsWallpaper(self.target)
+		pass
 
 	# Download informations from the Bing site in JSON
-	def getJSON(self):
+	def get_json(self):
 		return json.load(urllib2.urlopen(self.data))
 
 	# Set downloaded picture as a wallpaper
-	def setAsWallpaper(self, fileurl):
-		os.system('gsettings set org.gnome.desktop.background picture-uri "file://%s"' % fileurl)
+	def set_as_wallpaper(self):
+		url = self.domain % self.get_json()['images'][0]['url']
+		name = url.split('/')[-1]
+		self.target = '%s/%s/%s' % (os.path.dirname(os.path.abspath(__file__)), self.directory, name)
+
+		# check if file exists
+		if not os.path.isfile(self.target):
+			self.save(url)
+
+		os.system('gsettings set org.gnome.desktop.background picture-uri "file://%s"' % self.target)
 
 	# Save current Bing wallpaper
 	def save(self, url):
@@ -45,3 +40,4 @@ class BingBackground:
 
 # Run application
 bb = BingBackground()
+bb.set_as_wallpaper()
