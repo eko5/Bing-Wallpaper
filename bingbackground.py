139bee3362edd611bb846f247d7a052dce8873ab
Piotr Filipek <Piotrek290@gmail.com>
2015-09-17 11:37:46 +0200

    Added condition to check that file exists

--- bingbackground.py
+++ bingbackground.py
@@ -9,13 +9,18 @@ import json, urllib2, os
 class BingBackground:
 	domain = "http://www.bing.com%s"
 	data = domain % "/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+	target = ''
+	directory = 'wallpapers'
 
 	def __init__(self):
-		# print self.command % 'filename.jpg'
-		imgurl = self.domain % self.getJSON()['images'][0]['url']
-		imgname = imgurl.split('/')[-1]
-		target = self.save(imgurl, imgname)
-		self.setAsWallpaper(target)
+		url = self.domain % self.getJSON()['images'][0]['url']
+		name = url.split('/')[-1]
+		self.target = '%s/%s/%s' % (os.path.dirname(os.path.abspath(__file__)), self.directory, name)
+		
+		# check if file exists
+		if os.path.isfile(self.target):
+			self.save(url)
+			self.setAsWallpaper(self.target)
 
 	# Download informations from the Bing site in JSON
 	def getJSON(self):
@@ -27,13 +32,11 @@ class BingBackground:
 		os.system('gsettings set org.gnome.desktop.background picture-uri "file://%s"' % fileurl)
 
 	# Save current Bing wallpaper
-	def save(self, url, filename, directory='wallpapers'):
+	def save(self, url):
 		imgfile = urllib2.urlopen(url)
-		target = '%s/%s/%s' % (os.path.dirname(os.path.abspath(__file__)), directory, filename)
-		output = open(target, 'wb')
+		output = open(self.target, 'wb')
 		output.write(imgfile.read())
 		output.close()
-		return target
 
 # Run application
 bb = BingBackground()
