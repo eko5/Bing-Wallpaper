ef418eed302b3f8a6176f387fdbf5e3004d70d0e
Piotr Filipek <Piotrek290@gmail.com>
2015-09-17 13:44:16 +0200

    Replaced tabs to spaces

--- bingbackground.py
+++ bingbackground.py
@@ -7,36 +7,36 @@ import json, urllib2, os
 # Class that download current wallpaper from the Bing
 # site and sets its as a desktop wallpaper.
 class BingBackground:
-	domain = "http://www.bing.com%s"
-	data = domain % "/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
-	target = ''
-	directory = 'wallpapers'
+    domain = "http://www.bing.com%s"
+    data = domain % "/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+    target = ''
+    directory = 'wallpapers'
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	# Download informations from the Bing site in JSON
-	def get_json(self):
-		return json.load(urllib2.urlopen(self.data))
+    # Download informations from the Bing site in JSON
+    def get_json(self):
+        return json.load(urllib2.urlopen(self.data))
 
-	# Set downloaded picture as a wallpaper
-	def set_as_wallpaper(self):
-		url = self.domain % self.get_json()['images'][0]['url']
-		name = url.split('/')[-1]
-		self.target = '%s/%s/%s' % (os.path.dirname(os.path.abspath(__file__)), self.directory, name)
+    # Set downloaded picture as a wallpaper
+    def set_as_wallpaper(self):
+        url = self.domain % self.get_json()['images'][0]['url']
+        name = url.split('/')[-1]
+        self.target = '%s/%s/%s' % (os.path.dirname(os.path.abspath(__file__)), self.directory, name)
 
-		# check if file exists
-		if not os.path.isfile(self.target):
-			self.save(url)
+        # check if file exists
+        if not os.path.isfile(self.target):
+            self.save(url)
 
-		os.system('gsettings set org.gnome.desktop.background picture-uri "file://%s"' % self.target)
+        os.system('gsettings set org.gnome.desktop.background picture-uri "file://%s"' % self.target)
 
-	# Save current Bing wallpaper
-	def save(self, url):
-		imgfile = urllib2.urlopen(url)
-		output = open(self.target, 'wb')
-		output.write(imgfile.read())
-		output.close()
+    # Save current Bing wallpaper
+    def save(self, url):
+        imgfile = urllib2.urlopen(url)
+        output = open(self.target, 'wb')
+        output.write(imgfile.read())
+        output.close()
 
 # Run application
 bb = BingBackground()
